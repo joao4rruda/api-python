bac97f8658980ee8197380bbee0f2171a9bffa80
joao4rruda <joao.arruda.corp@gmail.com>
2025-03-20 15:34:33 -0300

    [ DATABASE  ] Select

--- db.py
+++ db.py
@@ -2,4 +2,8 @@ import pyodbc
 
 conn = pyodbc.connect('DRIVER={SQL Server};SERVER=172.16.0.41;DATABASE=DW_TEXNEO_HOMOLOG;UID=ext.lizardti.01;PWD=rzQUB%YjT&pSg6')
 
-print("db connected")
+cursor = conn.cursor()
+cursor.execute("select * from item")
+
+for row in cursor.fetchall():
+    print(row)
